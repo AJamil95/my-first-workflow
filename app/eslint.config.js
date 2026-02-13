699cd33a9c58abed9d20f29205e0c1f6a5824513
AJamil95 <armin95jamil@gmail.com>
2026-02-13 11:55:28 -0400

    fix exercise 7

---
+++ app/eslint.config.js
@@ -0,0 +1,19 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        __dirname: "readonly",
+      },
+    },
+  },
+];
